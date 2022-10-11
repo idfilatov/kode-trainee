a5e2a88bf1568d97862df69b1d668fb843a975b4
Ivan Filatov <39filatik@gmail.com>
2022-10-11 22:10:54 +0200

    Ready css for worker details page
    about 2.5h

--- src/components/WorkerDetailsPage.js
+++ src/components/WorkerDetailsPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
-import { updateWorkers } from './../utils'
+import { updateWorkers, departmentsToRussian, dateToHuman, getYearsOld } from './../utils'
 
 
 class WorkerDetailsPage extends React.Component {
@@ -30,18 +30,39 @@ class WorkerDetailsPage extends React.Component {
 
         return (
             <div>
-                <Link to={'/'}>На главную</Link>
-                <br />
-                Worker Details Page
-                <br />
-                <div className='worker-avatar'>
-                    <img src={currentWorker?.avatarUrl} alt='' />
+                <div className='worker-details-header'>
+                    <div className='worker-details-header-back'>
+                        <Link to={'/'}><i class="material-icons" style={{ 'color': '#050510' }}>chevron_left</i></Link>
+                    </div>
+                    <div className='worker-details-header-avatar-holder'>
+                        <img className='worker-details-header-avatar' src={currentWorker?.avatarUrl} alt='' />
+                    </div>
+                    <div className='worker-details-header-personal-info'>
+                        <div className='worker-details-header-name'>
+                            {`${currentWorker?.firstName ?? ''} ${currentWorker?.lastName ?? ''}`}
+                        </div>
+                        <div className='worker-details-header-tag'>
+                            {`${currentWorker?.userTag ?? ''}`}
+                        </div>
+                    </div>
+                    <div className='worker-details-header-department'>
+                        {`${departmentsToRussian[currentWorker?.department] ?? ''}`}
+                    </div>
                 </div>
-                <div className='worker-details'>
-                    {`${currentWorker?.firstName ?? ''} ${currentWorker?.lastName ?? ''} (${currentWorker?.userTag ?? ''})`}
-                    <br />
-                    {`${currentWorker?.position ?? ''}`}
-
+                <div className='worker-details-body'>
+                    <div className='worker-details-line'>
+                        <i class="material-symbols-outlined" style={{ 'color': '#050510' }}>star</i>
+                        <div className='worker-details-info'>
+                            {dateToHuman(currentWorker?.birthday ?? '', false)}
+                        </div>
+                    </div>
+                    <hr />
+                    <div className='worker-details-line'>
+                        <i class="material-symbols-outlined" style={{ 'color': '#050510' }}>call</i>
+                        <div className='worker-details-info'>
+                            {`${currentWorker?.phone ?? ''}`}
+                        </div>
+                    </div>
                 </div>
             </div>
         )
--- src/utils.js
+++ src/utils.js
@@ -137,22 +137,26 @@ export function reorderWorkers(sortedWorkers) {
     return workers
 }
 
-export function dateToHuman(date) {
+export function dateToHuman(date, short = true) {
+
+    if (!date) { return '' }
+
     const monthNames = {
-        '01': 'янв',
-        '02': 'фев',
-        '03': 'мар',
-        '04': 'апр',
+        '01': 'января',
+        '02': 'февраля',
+        '03': 'марта',
+        '04': 'апреля',
         '05': 'май',
-        '06': 'июн',
-        '07': 'июл',
-        '08': 'авг',
-        '09': 'сен',
-        '10': 'окт',
-        '11': 'ноя',
-        '12': 'дек',
+        '06': 'июня',
+        '07': 'июля',
+        '08': 'августа',
+        '09': 'сентября',
+        '10': 'октября',
+        '11': 'ноября',
+        '12': 'декабря',
 
     }
-    const [year, month, day] = date.split('-');
-    return `${parseInt(day)} ${monthNames[month]}`
+    const [year, month, day] = date?.split('-');
+    const humanDate = short ? `${parseInt(day)} ${monthNames[month].slice(0, 3)}` : `${parseInt(day)} ${monthNames[month]} ${year}`
+    return humanDate
 }
